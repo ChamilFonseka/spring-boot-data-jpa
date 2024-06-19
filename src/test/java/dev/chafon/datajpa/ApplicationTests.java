42c0535435975a7abc380667397fda6f3e6b2745
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-06-19 22:23:58 +0530

    added basic data-jpa configurations

--- src/test/java/dev/chafon/datajpa/ApplicationTests.java
+++ src/test/java/dev/chafon/datajpa/ApplicationTests.java
@@ -1,15 +1,27 @@
 package dev.chafon.datajpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private PostgreSQLContainer<?> postgreSQLContainer;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testPostgresStatus() {
+		assertThat(postgreSQLContainer.isCreated()).isTrue();
+		assertThat((postgreSQLContainer.isRunning())).isTrue();
+	}
 }
