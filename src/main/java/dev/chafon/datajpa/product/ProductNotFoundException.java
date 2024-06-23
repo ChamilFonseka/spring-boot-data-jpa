116b6eccf13437c45623bfe5ee35f7b0e542d1a8
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-06-23 21:05:18 +0530

    added exception handlers

--- src/main/java/dev/chafon/datajpa/product/ProductNotFoundException.java
+++ src/main/java/dev/chafon/datajpa/product/ProductNotFoundException.java
@@ -1,10 +1,6 @@
 package dev.chafon.datajpa.product;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-class ProductNotFoundException extends RuntimeException {
+public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(Integer id) {
         super("Could not find product with id : " + id);
     }
