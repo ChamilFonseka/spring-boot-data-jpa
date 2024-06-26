b9550d5593dbd7454bce23329652820f6d64daa5
Chamil Fonseka <Chamil.Fonseka@cambio.lk>
2024-06-26 20:15:20 +0530

    made OrderRepository public

--- src/main/java/dev/chafon/datajpa/customer/CustomerRepository.java
+++ src/main/java/dev/chafon/datajpa/customer/CustomerRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CustomerRepository extends BaseRepository<Customer, Integer> {
-
     @Query("select c from Customer c join fetch c.address")
     List<Customer> findAllWithAddresses();
 }
