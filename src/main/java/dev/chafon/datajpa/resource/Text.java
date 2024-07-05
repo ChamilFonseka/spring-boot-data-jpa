b3259caf9fba6a05d91d72f452b5422322ffa36a
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-07-05 23:04:46 +0530

    added the sub resource types

---
+++ src/main/java/dev/chafon/datajpa/resource/Text.java
@@ -0,0 +1,20 @@
+package dev.chafon.datajpa.resource;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "texts")
+public class Text extends Resource {
+  private String content;
+}
