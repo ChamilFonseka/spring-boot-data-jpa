0416db3dce689bf64c427e12033efb80cc651ea0
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-07-08 10:47:21 +0530

    added postgres container test
    # Conflicts:
    #	src/test/java/dev/chafon/datajpa/SpringBootDataJpaApplicationTests.java
    #	src/test/java/dev/chafon/datajpa/TestcontainersConfiguration.java

---
+++ src/test/java/dev/chafon/datajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,24 @@
+package dev.chafon.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+	@Autowired
+	private PostgreSQLContainer<?> postgreSQLContainer;
+
+	@Test
+	void contextLoads() {
+		assertThat(postgreSQLContainer.isCreated()).isTrue();
+		assertThat((postgreSQLContainer.isRunning())).isTrue();
+	}
+
+}
