ffe7c335f3dade4a3a2b54a533c848e8311494a9
Chamil Fonseka <fonseka.chamil@gmail.com>
2024-07-08 10:55:55 +0530

    configured spotless
    # Conflicts:
    #	src/main/java/dev/chafon/datajpa/SpringBootDataJpaApplication.java
    #	src/test/java/dev/chafon/datajpa/TestSpringBootDataJpaApplication.java
    #	src/test/java/dev/chafon/datajpa/TestcontainersConfiguration.java

---
+++ src/test/java/dev/chafon/datajpa/TestSpringBootDataJpaApplication.java
@@ -0,0 +1,12 @@
+package dev.chafon.datajpa;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSpringBootDataJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.from(SpringBootDataJpaApplication::main)
+        .with(TestcontainersConfiguration.class)
+        .run(args);
+  }
+}
